9354318c62ab6ffe70ecca5dae15b898090a6f4a
cindychanster <90227210+cindychanster@users.noreply.github.com>
2022-05-23 20:16:56 -0400

    update buttons

--- js/index.js
+++ js/index.js
@@ -7,3 +7,10 @@ $('#logo').on('mouseover', function(){
 $('#logo').on('mouseout', ()=>{
   $('#logo').attr('src','images/Logo.png')
 });
+$('#download').on('click', ()=>{
+  window.open('https://docs.google.com/document/d/1JKr2M_B_MdFMmxm_MfvdrMhCl4TxC7NkYxTTeC7W1oY/edit?usp=sharing','Download');
+})
+$('.linkto').on('click', function(){
+  const url = $(this).data('url');
+  location.href = url;
+})
